b0c335742ab0bee4225fa633a989ff644fe5b9e2
M <23571113m@gmail.com>
2018-06-07 19:51:17 -0400

    Lab0 and Lab1

---
+++ lab0.R
@@ -0,0 +1,28 @@
+myfun = function() {
+  print("Hello world!")
+}
+myfun()
+?hist
+5 * 2 + 3 * 2
+exp(-5)
+pi
+log(5)
+log(1000, base = 10)
+5 %% 3
+10 %/% 3
+r = 5
+r
+circ = 10*2*pi
+objects()
+weights = c(100, 115, 206, 225, 130, 130, 132, 141, 242, 123, 275, 178, 180, 181, 175)
+length(weights)
+min(weights)
+max(weights)
+kgvector = weights / 2.2
+sum(weights)
+mean(weights)
+setwd("~/Desktop/MiriamProgramming/r/r_practice")
+mytab = read.table("latlong.txt", header=T, as.is=T)
+head(mytab)
+dim(mytab)[1]
+names(mytab)
---
+++ lab1.R
@@ -0,0 +1,39 @@
+rm(list=ls())
+mytab = read.table("latlong.txt", header=T, as.is=T)
+mytab[1,]
+mytab[,dim(mytab)[2]]
+mynewtab = mytab[,-3]
+plot(mynewtab$long, sin(mynewtab$lat*pi/180), pch=16)
+myindices = which(mytab$continent.region=="Africa")
+mylongs = mytab$long[myindices]
+mylats = mytab$lat[myindices]
+plot(mylongs, sin(mylats*pi/180), pch=16)
+plot(mytab$long[which(mytab$continent.region=="Africa" | mytab$continent.region=="Europe")], sin(mytab$lat[which(mytab$continent.region=="Africa" | mytab$continent.region=="Europe")]*pi/180), pch=16)
+a = c(1, 1, 0)
+length(which(a==0))
+rm(a)
+length(which(mytab$long>=0 & mytab$long<=100))
+weights = c(100, 115, 206, 225, 130, 130, 132, 141, 242, 123, 275, 178, 180, 181, 175)
+weights_kg = weights / 2.2
+heights = c(60, 61, 72, 70, 60, 65, 65, 65, 67, 70, 74, 68, 73, 73, 73)
+sexes = c("F", "F", "M", "M", "F", "F", "F", "M", "M", "F", "M", "F", "M", "M", "M")
+heights_m = heights / 39.37
+min(heights_m)
+bmis = weights_kg / (heights_m^2)
+bmis[3]
+mean(bmis)
+sum(bmis) / length(bmis)
+bmis[c(1, 4, 12)]
+which(bmis > 18.5 & bmis < 24.9)
+which(weights==130)
+which(sexes=="M" & (bmis<20 | bmis>30))
+#My first comment
+ls()
+mydf = data.frame(weights, heights, sexes)
+mydf$heights[4]
+mydf[4,2]
+mydf[c(1, 3, 10),]
+mydf[7:11,]
+surveydata = read.table("lab1.surveydata.txt", header=T, as.is=T, sep="\t")
+head(surveydata)
+length(which(surveydata$sex=="M"))
