f5096c29db60f31cd13c4e6425e51b98c3c77786
Liebman <JLiebman@hks.internal>
2018-06-08 14:13:53 -0400

    Worked on Lab2

---
+++ Lab2.R
@@ -0,0 +1,40 @@
+for (i in 1:10) {
+  print(paste("i=",i))
+  print(paste("i+1=",i+1))
+}
+getwd()
+setwd("Desktop/MiriamProgramming/r/r_practice")
+surveydata = read.table("lab1.surveydata.txt", header=T, as.is=T)
+for (i in c(5:10, 20)) {
+  print(surveydata[i,])
+}
+length(which(surveydata$mothers_height_inches!="NA"))
+for (col in 1:2) {
+  print(mean(surveydata[,col][which(surveydata[,col]!="NA")]))
+}
+myranges = function(dataframe) {
+  for (i in 1:dim(dataframe)[2]) {
+    print(names(dataframe)[i])
+    numbers = which(dataframe[,i]!="NA")
+    a = max(dataframe[,i][numbers])
+    b = min(dataframe[,i][numbers])
+    print(a - b)
+  }
+  rm(numbers)
+  rm(a)
+  rm(b)
+}
+myranges(surveydata)
+myaverage = function(v) {
+  s = sum(v)
+  return(s / length(v))
+}
+myaverage(surveydata$fathers_height_inches[which(surveydata$fathers_height_inches!="NA")])
+myaverage(surveydata$siblings)
+avgheight = myaverage(surveydata$height_inches)
+colmin = function(dataframe, col) {
+  return(min(dataframe[,col]))
+}
+colmin(surveydata, 1)
+hist(surveydata$height_inches, col="red", xlim=c(48, 99), main="heights in inches", xlab="inches")
+boxplot(surveydata$height_inches[which(surveydata$sex=="F")], surveydata$height_inches[which(surveydata$sex=="M")])
